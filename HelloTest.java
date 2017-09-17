03f6cf4c4e9c0e18a7f3ea9daed59c46bcf741b7
manohar <manoharkrishna.g@gmail.com>
2017-09-17 17:41:16 +0530

    commit for git

--- HelloTest.java
+++ HelloTest.java
@@ -2,8 +2,8 @@ import java.io.*;
 class HelloTest {
 	public static void main(String[] args) {
 	for (int j=1;j<=10;j++){
-		System.out.println("hello world");
-
+		System.out.println("hello world git");
+System.out.println("commit for git");
 	}
 	}
 }
